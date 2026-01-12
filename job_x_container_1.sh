7a458e06c4aa5e3be8c9db3a9ceda79cca0d816b
KUOJENFENG <91510561+KUOJENFENG@users.noreply.github.com>
2026-01-12 16:07:51 +0100

    Add files via upload
    script

---
+++ job_x_container_1.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+#SBATCH -J fenics_simulation
+#SBATCH -N 4
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=32GB
+#SBATCH --time=12:00:00
+#SBATCH -A plggemini2025-cpu
+#SBATCH -p plgrid
+#SBATCH --output="/net/pr2/projects/plgrid/plgggemini/perfusion_and_tissue_damage/out_k.out"
+#SBATCH --error="/net/pr2/projects/plgrid/plgggemini/perfusion_and_tissue_damage/err_k.err"
+
+BASE_DIR="/net/pr2/projects/plgrid/plgggemini/perfusion_and_tissue_damage"
+CONTAINER="$BASE_DIR/perfusion_and_tissue_damage.sif"
+BLOODFLOW_DIR="$BASE_DIR/bloodflow/DataFiles/DefaultPatient"
+SCRIPT_DIR="$BASE_DIR/perfusion"    
+
+singularity exec \
+    --bind "$BASE_DIR:/mnt/project" \
+    --cleanenv \
+    "$CONTAINER" \
+    bash -c "
+        export PATH=\$HOME/.local/bin:\$PATH
+        export PYTHONPATH=\$HOME/.local/lib/python3.*/site-packages:\$PYTHONPATH
+        export PYTHONPATH=/mnt/project/bloodflow/Blood_Flow_1D:$PYTHONPATH
+        
+	python3 -m pip install --user --no-cache-dir networkx
+	python3 -m pip install --user --no-cache-dir vtk
+	python3 -m pip install --user --no-cache-dir mgmetis
+	python3 -m pip install --user --no-cache-dir pandas
+	python3 -m pip install --user --no-cache-dir numpy==1.24.4
+        
+        cd /mnt/project/
+        cp -TR ./bloodflow/DataFiles/DefaultPatient_old "./perfusion/patient_0/"
+        
+	
+    "
+
